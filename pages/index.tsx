187075afab5fa891cd476ba3ac36962ce067a6ed
Otavio Jacobi <otaviojacobi@gmail.com>
2024-12-15 18:46:42 -0300

    add initial page with a logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+
 function Home() {
-    return <h1>Page in construction</h1>;
+  return (
+    <div className="flex items-center justify-center h-screen bg-[#f6db98]">
+      <Image
+        src="/doggo.jpg"
+        alt="Logo de cachorro e gato felizes"
+        width={650}
+        height={520}
+      />
+    </div>
+  );
 }
 
 export default Home;
